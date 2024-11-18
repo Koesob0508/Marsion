b0bb1ed3fe131e56223a5d8b6c544280d0b8673f
Koesob <epi3706@khu.ac.kr>
2024-11-18 15:53:22 +0900

    Practice

---
+++ Assets/Scripts/Practice/NetworkCommandManager.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Netcode;
+
+namespace Practice
+{
+    public class NetworkCommandManager : NetworkBehaviour
+    {
+        private CommandManager _commandManager;
+
+        private void Awake()
+        {
+            _commandManager = new CommandManager();
+        }
+
+        [ServerRpc]
+        public void UseCardServerRpc(ulong playerID, string cardID)
+        {
+            var player = FindPlayerByID(playerID);
+            var card = FindCardByID(cardID);
+
+            if(player != null && card != null)
+            {
+                var command = new UseCardCommand(card, player);
+                _commandManager.ExecuteCommand(command);
+
+                UseCardClientRpc(playerID, cardID);
+            }
+        }
+
+        [ClientRpc]
+        public void UseCardClientRpc(ulong playerID, string cardID)
+        {
+            if (IsServer) return;
+
+            var player = FindPlayerByID(playerID);
+            var card = FindCardByID(cardID);
+
+            if(player != null && card != null)
+            {
+                var command = new UseCardCommand(card, player);
+                _commandManager.ExecuteCommand(command);
+            }
+        }
+
+        private Player FindPlayerByID(ulong playerID)
+        {
+            return null;
+        }
+
+        private string FindCardByID(string cardID)
+        {
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Practice/NetworkGameManager.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Practice
+{
+    public class NetworkGameManager : NetworkBehaviour
+    {
+        public static NetworkGameManager Instance;
+
+        private void Awake()
+        {
+            if (Instance == null) Instance = this;
+            else Destroy(gameObject);
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void StartHost()
+        {
+            NetworkManager.Singleton.StartHost();
+            Debug.Log("Host started");
+        }
+
+        public void StartClient()
+        {
+            NetworkManager.Singleton.StartClient();
+            Debug.Log("Client started");
+        }
+
+        public void Disconnect()
+        {
+            NetworkManager.Singleton.Shutdown();
+            Debug.Log("Disconnected");
+        }
+    }
+
+}
