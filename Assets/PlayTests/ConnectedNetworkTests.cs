ed923fef1210f376e42c1943dd350d6bcdd5fda2
Koesob <epi3706@khu.ac.kr>
2024-12-16 10:27:08 +0900

    테스트

--- Assets/PlayTests/ConnectedNetworkTests.cs
+++ Assets/PlayTests/ConnectedNetworkTests.cs
@@ -66,8 +66,8 @@ namespace Marsion.Tests
                 .Setup(GM => GM.NetworkEx)
                 .Returns(guestNetworkEx);
 
-            HostManagers.Object.NetworkEx.StartHost();
-            GuestManagers.Object.NetworkEx.StartClient();
+            //HostManagers.Object.NetworkEx.StartHost();
+            //GuestManagers.Object.NetworkEx.StartClient();
 
             yield return new WaitForSeconds(0.5f);
         }
@@ -94,7 +94,10 @@ namespace Marsion.Tests
         [UnityTest]
         public IEnumerator SetUpTest()
         {
-            yield return null;
+            HostManagers.Object.NetworkEx.StartHost();
+            GuestManagers.Object.NetworkEx.StartClient();
+
+            yield return new WaitForSeconds(0.5f);
 
             Debug.Log(HostManagers.Object.NetworkEx.LocalClientID);
             Debug.Log(GuestManagers.Object.NetworkEx.LocalClientID);
@@ -108,7 +111,10 @@ namespace Marsion.Tests
         [UnityTest]
         public IEnumerator TearDownTest()
         {
-            yield return null;
+            HostManagers.Object.NetworkEx.StartHost();
+            GuestManagers.Object.NetworkEx.StartClient();
+
+            yield return new WaitForSeconds(0.5f);
 
             Assert.IsTrue(HostManagers.Object.NetworkEx.IsHost);
             Assert.IsTrue(GuestManagers.Object.NetworkEx.IsClient);
@@ -117,14 +123,82 @@ namespace Marsion.Tests
         }
 
         [UnityTest]
+        public IEnumerator OnClientConnected_Callback_Should_Invoke()
+        {
+            // OnClientConnected 핸들러 등록
+            bool isClientConnected = false;
+            hostNetwork.OnClientConnectedCallback += (clientID) =>
+            {
+                Debug.Log($"Client connected with ID: {clientID}");
+            };
+
+            HostManagers.Object.NetworkEx.OnClientConnected += (clientID) =>
+            {
+                Debug.Log($"Client connected with ID: {clientID}");
+                isClientConnected = true;
+            };
+
+            yield return new WaitForSeconds(1f);
+
+            HostManagers.Object.NetworkEx.StartHost();
+
+            yield return new WaitForSeconds(1f);
+
+            GuestManagers.Object.NetworkEx.StartClient();
+
+            yield return new WaitForSeconds(1f);
+
+            //var connectedClients = HostManagers.Object.NetworkEx.ConnectedClientsIDs;
+            //foreach (var clientId in connectedClients)
+            //{
+            //    Debug.Log($"Existing connected client ID: {clientId}");
+            //    isClientConnected = true; // 이미 연결된 클라이언트를 검증
+            //}
+
+            // 검증
+            Assert.IsTrue(GuestManagers.Object.NetworkEx.IsClient, "Guest is not properly connected.");
+            Assert.IsTrue(isClientConnected, "OnClientConnected callback was not invoked.");
+        }
+
+        [UnityTest]
         public IEnumerator Host_Send_Client_Should_Received()
         {
+            HostManagers.Object.NetworkEx.StartHost();
+            GuestManagers.Object.NetworkEx.StartClient();
+
+            bool expectedBool = false;
             string expectedString = "TestMessage";
             int expectedInt = 1234;
 
-            yield return null;
+            yield return new WaitForSeconds(0.5f);
 
-            
+            ulong targetID = GuestManagers.Object.NetworkEx.LocalClientID;
+
+            string receivedString = "";
+            int receivedInt = 0;
+
+            GuestManagers.Object.NetworkEx.SubscribeMessage("Host", (senderID, reader) =>
+            {
+                expectedBool = true;
+                reader.ReadValueSafe(out receivedString);
+                reader.ReadValueSafe(out receivedInt);
+            });
+
+            System.Action<FastBufferWriter> messageWriter = (writer) =>
+            {
+                writer.WriteValueSafe(expectedString);
+                writer.WriteValueSafe(expectedInt);
+            };
+
+            HostManagers.Object.NetworkEx.SendMessage("Host", targetID, messageWriter, NetworkDelivery.Reliable);
+
+            yield return new WaitForSeconds(0.5f); // 메시지 처리 시간 대기
+
+            Assert.IsTrue(expectedBool);
+            Assert.AreEqual(expectedString, receivedString);
+            Assert.AreEqual(expectedInt, receivedInt);
+
+            yield break;
         }
     }
 }
