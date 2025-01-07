86fd5e36723711d8573c875415b21a45ba29a097
Koesob <epi3706@khu.ac.kr>
2025-01-07 14:16:10 +0900

    동기화용

--- Assets/Scripts/03. GameLogic/Player.cs
+++ Assets/Scripts/03. GameLogic/Player.cs
@@ -1,9 +1,6 @@
-﻿using Marsion.Tool;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
 
 namespace Marsion
 {
